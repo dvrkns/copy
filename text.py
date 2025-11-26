05d640112b7597602b547d463acd9922e54eb3f0
dvrkns <deathdestroyingworlds@gmail.com>
2025-11-26 23:45:03 +0300

    Update text.py

--- text.py
+++ text.py
@@ -1,10 +1,1 @@
-# Проверяем, есть ли хотя бы одна пара
-        has_pairs = any(pair is not None for pair in pairs_raw)
 
-        if not has_pairs:
-            # Если пар нет, выводим сообщение о выходных
-            lines.append("")
-            lines.append("<b>Удачных выходных!</b>")
-            return "\n".join(lines)
-
-        lines.append("")
